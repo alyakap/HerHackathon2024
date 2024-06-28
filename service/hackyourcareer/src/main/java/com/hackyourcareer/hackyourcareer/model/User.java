5a383acbec6caaaee63e9ed260e63724f671608c
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2024-06-28 22:31:36 +0200

    add backend setup

---
+++ service/hackyourcareer/src/main/java/com/hackyourcareer/hackyourcareer/model/User.java
@@ -0,0 +1,4 @@
+package com.hackyourcareer.hackyourcareer.model;
+
+
+public record User(Long id, String username) {}
---
+++ service/hackyourcareer/src/test/java/com/hackyourcareer/hackyourcareer/HackyourcareerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackyourcareer.hackyourcareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackyourcareerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
