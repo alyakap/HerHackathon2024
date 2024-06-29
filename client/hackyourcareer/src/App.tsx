94ed16ce50892243496e54873345f75b06c32032
Alya Kapusuz <kapusuzz@delawareconsulting.com>
2024-06-29 14:07:09 +0200

    last commit before merge

--- client/hackyourcareer/src/App.tsx
+++ client/hackyourcareer/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SigninSide from "./Components/signin/SignInSide";
+import MenteeSignup from "./Components/form/MenteeSignup";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/signin" element={<SigninSide />} />
+          <Route path="/signup" element={<MenteeSignup />} />
           {/* Add more routes if necessary */}
         </Routes>
       </div>
