a66ce5771803dc3f70824e10020922b478288385
juli <julietatolm@gmail.com>
2024-06-29 09:29:14 +0200

    add dependencies

--- service/hackyourcareer/src/main/java/com/hackyourcareer/hackyourcareer/model/User.java
+++ service/hackyourcareer/src/main/java/com/hackyourcareer/hackyourcareer/model/User.java
@@ -1,4 +1,8 @@
 package com.hackyourcareer.hackyourcareer.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "users")
 public record User(Long id, String username) {}
